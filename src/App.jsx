3c39a899df8ee365ae0eaabc5350b8db491a3514
vitorluizms <vitor.luiz.eer@gmail.com>
2023-05-29 09:48:38 -0300

    requisição de lista de filmes e adicionada na HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,46 @@
-import styled from "styled-components"
-import HomePage from "./pages/HomePage/HomePage"
-import SeatsPage from "./pages/SeatsPage/SeatsPage"
-import SessionsPage from "./pages/SessionsPage/SessionsPage"
-import SuccessPage from "./pages/SuccessPage/SuccessPage"
+import styled from "styled-components";
+import HomePage from "./pages/HomePage/HomePage";
+import SeatsPage from "./pages/SeatsPage/SeatsPage";
+import SessionsPage from "./pages/SessionsPage/SessionsPage";
+import SuccessPage from "./pages/SuccessPage/SuccessPage";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-    return (
-        <>
-           <NavContainer>CINEFLEX</NavContainer>
+  axios.defaults.headers.common["Authorization"] = "4wC4CuuPziLhfX4rSqYJaE9Z";
 
-            <HomePage />
-            {/* <SeatsPage /> */}
-            {/* <SessionsPage /> */}
-            {/* <SuccessPage /> */}
-        </>
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <NavContainer>CINEFLEX</NavContainer>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/sessoes/:idFilme" element={<SeatsPage />} />
+          <Route path="/assentos/:idSessao" element={<SessionsPage />} />
+          <Route path="/sucesso" element={<SuccessPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 const NavContainer = styled.div`
-    width: 100%;
-    height: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #C3CFD9;
-    color: #E8833A;
-    font-family: 'Roboto', sans-serif;
-    font-size: 34px;
-    position: fixed;
-    top: 0;
-    a {
-        text-decoration: none;
-        color: #E8833A;
-    }
-`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #c3cfd9;
+  color: #e8833a;
+  font-family: "Roboto", sans-serif;
+  font-size: 34px;
+  position: fixed;
+  top: 0;
+  a {
+    text-decoration: none;
+    color: #e8833a;
+  }
+`;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,66 @@
-import styled from "styled-components"
+import { useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
 export default function HomePage() {
-    return (
-        <PageContainer>
-            Selecione o filme
+  const [movies, setMovies] = useState([]);
 
-            <ListContainer>
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-
-                <MovieContainer>
-                    <img src={"https://br.web.img2.acsta.net/pictures/22/05/16/17/59/5165498.jpg"} alt="poster"/>
-                </MovieContainer>
-            </ListContainer>
-
-        </PageContainer>
+  useEffect(() => {
+    const promise = axios.get(
+      "https://mock-api.driven.com.br/api/v8/cineflex/movies"
+    );
+    promise.then((answer) => {
+      setMovies(answer.data);
+    });
+    promise.catch((erro) => {alert('Ocorreu um erro!')});
+  }, []);
+  if (movies.length === 0){
+    return(
+        <PageContainer>Carregando...</PageContainer>
     )
-}
+  }
+  else {
+  return (
+    <PageContainer>
+      Selecione o filme
+      <ListContainer>
+        {movies.map((movie) => (<MovieContainer key={movie.id}> <img src={movie.posterURL} alt={movie.title}/>
+        </MovieContainer>))}
+      </ListContainer>
+    </PageContainer>
+  );
+}}
 
 const PageContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    font-family: 'Roboto';
-    font-size: 24px;
-    text-align: center;
-    color: #293845;
-    margin-top: 30px;
-    padding-top: 70px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: "Roboto";
+  font-size: 24px;
+  text-align: center;
+  color: #293845;
+  margin-top: 30px;
+  padding-top: 70px;
+`;
 const ListContainer = styled.div`
-    width: 330px;
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    padding: 10px;
-`
+  max-width: 330px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 10px;
+`;
 const MovieContainer = styled.div`
-    width: 145px;
-    height: 210px;
-    box-shadow: 0px 2px 4px 2px #0000001A;
-    border-radius: 3px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin: 10px;
-    img {
-        width: 130px;
-        height: 190px;
-    }
-`
+  width: 145px;
+  height: 210px;
+  box-shadow: 0px 2px 4px 2px #0000001a;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  img {
+    width: 130px;
+    height: 190px;
+  }
+`;
